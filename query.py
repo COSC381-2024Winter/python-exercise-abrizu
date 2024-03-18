792a1619d1c65e6e71586f8f0650add9c90de469
Aaron Brizuela <157730037+abrizu@users.noreply.github.com>
2024-03-18 01:06:12 +0000

    Add search function (Issue #5)

--- query.py
+++ query.py
@@ -7,6 +7,7 @@ def __menu__():
     while True:
         print("\n\n-- Movie Menu --")
         print("[l] : View movie list")
+        print("[s] : Search for a movie")
         print("[q] : Exit")
 
         menu = input("\nEnter an option: ")
@@ -17,7 +18,17 @@ def __menu__():
         elif menu == "l":
             for movie in movies._movies:
                 print(movie['name'])
+        elif menu == "s":
+            searchMovie = input("\nSearch for a movie: ")
+            __search__(searchMovie)
         else:
             print("Invalid option. Please, enter again: ")
+
+def __search__(search):
+    found_movies = movies.search_movies(search)
+    if found_movies:
+        print(", ".join(found_movies))
+    else:
+        print("No movies found.")
 
 __menu__()
